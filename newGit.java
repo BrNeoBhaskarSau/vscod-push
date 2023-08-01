d48f625e3c6cc79b2739b1632fee1b780de30818
Bhaskar Sau <130405181+BrNeoBhaskarSau@users.noreply.github.com>
2023-08-02 01:47:20 +0530

    second commit

---
+++ newGit.java
@@ -0,0 +1,17 @@
+public class newGit {
+    public static void main(String args[]){
+        int multiply = 2;
+        for(int i=1; i<=10; i++){
+            System.out.println(multiply+" X "+i+" = "+i*multiply);
+        }
+        for(int i=1; i<=10; i++){
+            System.out.println(multiply+" / "+i+" = "+i/multiply);
+        }
+        for(int i=1; i<=10; i++){
+            System.out.println(multiply+" + "+i+" = "+i+multiply);
+        }
+        for(int i=1; i<=10; i++){
+            System.out.println(multiply+" % "+i+" = "+i%multiply);
+        }
+    }
+}
